b4911f9f357d4ed0b87884754172aec143feb3e3
S M Bilal Arshad <syedmuhammadbilalarshad@gmail.com>
2022-08-25 23:00:48 +0500

    Updated Transaction list with Datepicker and delete method

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -5,13 +5,14 @@ import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> _userTransactions;
+  final Function _deleteTransaction;
 
-  const TransactionList(this._userTransactions);
+  const TransactionList(this._userTransactions, this._deleteTransaction);
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 300,
+      height: 550,
       child: _userTransactions.isEmpty
           ? Column(children: <Widget>[
               Text('No transactions added yet!'),
@@ -34,19 +35,24 @@ class TransactionList extends StatelessWidget {
                     horizontal: 5,
                   ),
                   child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Padding(
-                        padding: const EdgeInsets.all(6),
-                        child: FittedBox(
-                            child:
-                                Text('\$${_userTransactions[index].amount}')),
+                      leading: CircleAvatar(
+                        radius: 30,
+                        child: Padding(
+                          padding: const EdgeInsets.all(6),
+                          child: FittedBox(
+                              child:
+                                  Text('\$${_userTransactions[index].amount}')),
+                        ),
                       ),
-                    ),
-                    title: Text(_userTransactions[index].title),
-                    subtitle: Text(DateFormat.yMMMd()
-                        .format(_userTransactions[index].date)),
-                  ),
+                      title: Text(_userTransactions[index].title),
+                      subtitle: Text(DateFormat.yMMMd()
+                          .format(_userTransactions[index].date)),
+                      trailing: IconButton(
+                        icon: Icon(Icons.delete),
+                        color: Theme.of(context).errorColor,
+                        onPressed: () =>
+                            _deleteTransaction(_userTransactions[index].id),
+                      )),
                 );
               },
               itemCount: _userTransactions.length,
