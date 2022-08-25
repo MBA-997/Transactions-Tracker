45cd26cd05e63acbe2273e16adc59e8baae871aa
S M Bilal Arshad <syedmuhammadbilalarshad@gmail.com>
2022-08-25 23:01:45 +0500

    Updated Chart style

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -27,7 +27,7 @@ class Chart extends StatelessWidget {
         'day': DateFormat.E().format(weekDay).substring(0, 3),
         'amount': totalSum.roundToDouble(),
       };
-    });
+    }).reversed.toList();
   }
 
   double get totalSpending {
